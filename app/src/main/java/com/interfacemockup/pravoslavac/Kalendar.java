d5be04ce1f0bbbb5c4a5c47743392e75d9827c8a
Zeljko Bilbija <bilbijazeljko@gmail.com>
2020-02-05 13:04:18 +0100

    Dodat string arraj sa nazivima meseca

---
+++ app/src/main/java/com/interfacemockup/pravoslavac/Kalendar.java
@@ -0,0 +1,18 @@
+package com.interfacemockup.pravoslavac;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Kalendar {
+    Calendar kla = Calendar.getInstance();
+    
+    
+    boolean redniDAnUGodini) {
+        boolean lenient = kla.isLenient();
+        Date danas = new Date();
+
+        return lenient;
+    }
+
+
+}
